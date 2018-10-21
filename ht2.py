9f4add0654bb236bb246f10c79d436f3f3788e89
AntonS2k <44340806+AntonS2k@users.noreply.github.com>
2018-10-21 18:03:24 +0300

    Add files via upload

---
+++ ht2.py
@@ -0,0 +1,7 @@
+a = 1
+b = 2
+c = float (a+b)
+a = float (c-a)
+b = float (c-b)
+print('новое значение числа а', a)
+print('новое значение числа в', b)
---
+++ ht4.py
@@ -0,0 +1,7 @@
+a = int(input('Введите число '))
+b = int (a%2)
+if b == 0:
+    print(a*a)
+else:
+    print (a*2)
+
