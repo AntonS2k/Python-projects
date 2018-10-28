9ba612b1afa96c97306a834380488c406a501c59
AntonS2k <44340806+AntonS2k@users.noreply.github.com>
2018-10-28 18:49:43 +0200

    Add files via upload

--- HW3_task2.py
+++ HW3_task2.py
@@ -1,9 +1,10 @@
-num = int(input('Enter factorial n: '))
-if num < 0:
+n = int(input('Enter factorial n: '))
+f=1
+if n < 0:
     print("error - negative number")
-elif num == 0:
+elif n == 0:
         print("The factorial of 0 is 1")
 else:
-    for i in range(1, num, +1):
-        factorial = num * i
-print("The factorial of",num,"is",factorial)
+    for i in range(1, n+1):
+        f *= i
+print("The factorial of",n,"is",f)
