b70be1184cb041d388c1c603d1db925a5627c4d1
AntonS2k <44340806+AntonS2k@users.noreply.github.com>
2018-10-28 20:21:36 +0200

    Add files via upload

---
+++ HW3_task3.py
@@ -0,0 +1,8 @@
+ab = int(input("ab: "))
+bcca = int(input("bc=ca :"))
+for zvezda_ab in range(ab):
+    for zvezda_bcca in range(bcca):
+        for zvezda_bcca in range(bcca):
+            print('* ')
+    print()
+
