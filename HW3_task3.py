c836ec7197242778630bdb35ae5173cc18104691
AntonS2k <44340806+AntonS2k@users.noreply.github.com>
2018-10-29 14:13:21 +0200

    Add files via upload

--- HW3_task3.py
+++ HW3_task3.py
@@ -1,7 +1,7 @@
 n = int(input('введите число '))
 for a in range(0, n):
-    for b in range (0, n-a-1):
+    for b in range (0, a+1):
         print(end='')
-    for b in range(0, a+1):
+    for c in range(0, a+1):
         print('*', end='')
     print()
