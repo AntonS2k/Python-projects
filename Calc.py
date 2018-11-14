6d2536c464a719fa3bceaf189d92385fd6953ec7
AntonS2k <44340806+AntonS2k@users.noreply.github.com>
2018-11-14 13:35:42 +0200

    Add files via upload

--- Calc.py
+++ Calc.py
@@ -20,6 +20,7 @@ def div (a, b):
             loop = False
 
 
+
 import math as m
 
 MATH_OPERATION = {
@@ -31,20 +32,16 @@ MATH_OPERATION = {
 }
 
 def main():
-    x = float(input('>> '))
-    y = float(input('>> '))
-    oper  = input('Operation is: ')
-    res = MATH_OPERATION[oper](x , y)
-    loop  = True
-    while loop:
+    while True:
         try:
+            x = float(input('>> '))
+            y = float(input('>> '))
+            oper  = input('Operation is: ')
+            res = MATH_OPERATION[oper](x , y)
             if res:
                 print('Result:',res)
             elif res == 0:
-                print ('Result: 0')
-        except (ValueError) as error:
-            print('Error:', error)
-            print('Please try again')
-            print()
-    loop = False
+                    print ('Result: 0')
+        except ValueError:
+            print("Please type valid data")
 main()
