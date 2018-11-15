08a568898331ccd3ffb94d8185febe3fa4e42ead
AntonS2k <44340806+AntonS2k@users.noreply.github.com>
2018-11-15 17:02:04 +0200

    Add files via upload

---
+++ Calc — копия.py
@@ -0,0 +1,60 @@
+def add(a , b):
+    return a + b
+
+def sub(a , b):
+    return a - b
+
+def mul (a, b):
+    return a*b
+
+def div (a, b):
+    while True:
+        try:
+            print('Result:', a / b)
+            break
+        except (ValueError, ZeroDivisionError) as error:
+            print('Error:', error)
+            print('Please try again')
+            break
+
+
+import math as m
+
+TRIG_OPERATION = {
+    'cos': m.cos,
+    'sin': m.sin,
+    }
+
+MATH_OPERATION = {
+    '+': add,
+    '-': sub,
+    '*': mul,
+    '/': div,
+    'log': m.log,
+    '**': pow,
+    }
+
+
+oper = input('Operation is: ')
+x = float(input('>> '))
+
+if oper == MATH_OPERATION:
+        try:
+            y = float(input('>> '))
+            res = MATH_OPERATION[oper](x , y)
+            if res:
+                print('Result:',res)
+            elif res == 0:
+                print ('Result: 0')
+        except (ValueError, KeyError) as error:
+            print("Please type valid data")
+
+else:
+            try:
+                res_trig = TRIG_OPERATION[oper](x)
+                if res_trig:
+                    print('Result:', res_trig)
+                elif res_trig == 0 :
+                        print ('Result: 0')
+            except (ValueError, KeyError) as error:
+                print("Please type valid data")
